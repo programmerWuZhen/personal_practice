c52bfe0bfb72a1d497a2c2db2e09330ea2eb4474
ZhenWu <1634830342@qq.com>
2021-10-08 23:31:22 +0800

    增加了用于测试的文件夹，并测试了右移符号为算术右移

---
+++ WuZhen_project/Test/Test/源.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	/*  //²âÊÔÓÒÒÆ·ûºÅÎªËãÊõÓÒÒÆ»¹ÊÇÂß¼­ÓÒÒÆ
+	void testShiftR();
+	testShiftR();//µ÷ÓÃ ½áÂÛ ËãÊõÓÒÒÆ
+	*/
+}
+//²âÊÔÓÒÒÆ·ûºÅÊÇËãÊõÓÒÒÆ»¹ÊÇÂß¼­ÓÒÒÆ
+void testShiftR() {
+	int i = -1;
+	i = i >> 1;
+	printf("%x", i);
+}
